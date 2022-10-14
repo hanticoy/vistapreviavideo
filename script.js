633730e0ffb618e87e11bf8bb68daa89fef88699
hanticoy <hugo.anticoy.anticoy@gmail.com>
2022-10-14 15:57:51 -0300

    actualizacion de funcion

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-console.log("page loaded...");
+async function playVideo(videoElem) {
+    try {
+        await videoElem.play();
 
+    } catch (err) {
+        console.log('error')
+    }
+}
+
+function fnOnMouseOver(videoElem) {
+    if (videoElem.paused) {
+        
+        playVideo(videoElem);
+    } else {
+        videoElem.pause();
+    }
+}
+function fnOnMouseOut(obj) {
+    let video = document.getElementById("myVideoPlayer");
+    video.pause();
+    video.currentTime = 0;
+}
